61f534af57cc04a3f327ad4f38d4cd375ae7a52f
wise-guru <wise.guru123@gmail.com>
2022-09-20 22:16:20 -0400

    Format

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,5 +1,23 @@
-function GameEnd() {
-  return <div />;
+import { Link } from 'react-router-dom';
+import TimeDisplay from './TimeDisplay';
+
+function GameEnd(props) {
+  const { time } = props;
+  return (
+    <div className="bgModal">
+      <div className="modalContent">
+        <h1>You win!</h1>
+        <h2>
+          Time: <TimeDisplay time={time} />
+        </h2>
+        <div>
+          <button type="button" onClick={() => window.location.reload()}>
+            Play again
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default GameEnd;
