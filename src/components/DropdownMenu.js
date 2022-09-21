9f128fb036236e14d91c634ec4ee101d004268f3
wise-guru <wise.guru123@gmail.com>
2022-09-20 21:39:00 -0400

    Remove unused props

---
+++ src/components/DropdownMenu.js
@@ -0,0 +1,40 @@
+import { useRef } from 'react';
+import PB from '../Assets/bubblegum.jpg';
+import MagicMan from '../Assets/magicMan.webp';
+import BMO from '../Assets/BMO.webp';
+import DropdownField from './DropdownFields';
+
+function DropdownMenu({
+  characters = [
+    { name: 'Princess Bubblegum', character: 'bubblegum', image: PB, id: 1, clicked },
+    { name: 'Magic Man', character: 'magicMan', image: MagicMan, id: 2 },
+    { name: 'BMO', character: 'bmo', image: BMO, id: 3 }
+  ],
+  compareValues
+}) {
+  //   const characters = ({ ['Princess Bubblegum', 'BMO', 'Magic Man'], show, clickLocation, clicked;
+  //   const { x, y } = props;
+
+  //   const location = getCursorPosition();
+
+  return (
+    <div className="dropdownMenu" style={{ backgroundColor: 'white', color: 'red' }}>
+      <div className="targets">
+        {characters.map((character) => {
+          return (
+            <DropdownField
+              image={character.image}
+              name={character.name}
+              key={character.name}
+              compareValues={compareValues}
+              character={character.character}
+              id={character.id}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default DropdownMenu;
