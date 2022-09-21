939ad9ffc50320d80a3df00f8a80439bb2aeee03
wise-guru <wise.guru123@gmail.com>
2022-09-20 21:36:53 -0400

    Add field for each dropdown menu character

---
+++ src/components/DropdownFields.js
@@ -0,0 +1,31 @@
+import { useRef } from 'react';
+
+function DropdownField(props) {
+  const { name, image, clicked, compareValues, character, id } = props;
+
+  const buttonRef = useRef(null);
+
+  return (
+    <button
+      type="button"
+      key={name}
+      //   href="#menuitem"
+      className={`dropdownCharacterBtn ${character}Btn`}
+      ref={buttonRef}
+      onClick={(e) => {
+        compareValues(e, character);
+      }}
+      //   role="menuitem"
+    >
+      <div className="dropdownCharacter">
+        <div className="dropdownCharacterImage">
+          <img src={image} alt={name} />
+        </div>
+
+        <div>{name}</div>
+      </div>
+    </button>
+  );
+}
+
+export default DropdownField;
