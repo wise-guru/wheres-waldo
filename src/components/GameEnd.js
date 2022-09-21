de15d7459ea80370d752af1bd7e8c289f87fc5c0
wise-guru <wise.guru123@gmail.com>
2022-09-20 23:44:51 -0400

    Format

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import TimeDisplay from './TimeDisplay';
 
 function GameEnd(props) {
--- src/components/GameStartModal.js
+++ src/components/GameStartModal.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 function GameStartModal(props) {
   const { setTimerOn, setStartGameModal } = props;
   return (
