9131dee45dd10164cafdfa1b8437a5514477903d
wise-guru <wise.guru123@gmail.com>
2022-09-20 21:46:06 -0400

    Add starting modal to game

---
+++ src/components/GameStartModal.js
@@ -0,0 +1,25 @@
+import { Link } from 'react-router-dom';
+
+function GameStartModal(props) {
+  const { setTimerOn, setStartGameModal } = props;
+  return (
+    <div className="bgModal">
+      <div className="modalContent">
+        <h1>Where&apos;s Waldo: Adventure Time Edition</h1>
+        <div>Find Princess Bubblegum, BMO, and Magic Man as fast as you can!</div>
+        <div>
+          <button
+            type="button"
+            onClick={() => {
+              setStartGameModal(false);
+              setTimerOn(true);
+            }}>
+            Start Game
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GameStartModal;
