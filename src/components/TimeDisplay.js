58e2246c9f22e597108a53be26e82b71d727e0f5
wise-guru <wise.guru123@gmail.com>
2022-09-22 17:20:32 -0400

    Change font size

--- src/components/TimeDisplay.js
+++ src/components/TimeDisplay.js
@@ -1,11 +1,11 @@
 function TimeDisplay(props) {
   const { time } = props;
   return (
-    <h2 id="display">
+    <h3 id="display">
       <span>{`0${Math.floor((time / 60000) % 60)}`.slice(-2)}:</span>
       <span>{`0${Math.floor((time / 1000) % 60)}`.slice(-2)}:</span>
       <span>{`0${(time / 10) % 100}`.slice(-2)}</span>
-    </h2>
+    </h3>
   );
 }
 
